10868f8f4a9a7e33b336dfdac6a2101ed004f703
kyungnankim <implename@gmail.com>
2023-08-16 14:27:43 +0900

    FFFF

--- javascript/progressbar.js
+++ javascript/progressbar.js
@@ -46,7 +46,7 @@ function formatTime(secs) {
 }
 
 function setTitle(progress) {
-    var title = 'Beyond Fasion';
+    var title = 'Beyond Fashion';
 
     if (opts.show_progress_in_title && progress) {
         title = '[' + progress.trim() + '] ' + title;
